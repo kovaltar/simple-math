215ce35e9fc7f1e476b542d51e5ee88ca186e249
kovaltar <kovaltar@gmail.com>
2026-02-16 03:06:31 +0100

    Add operations

--- script.js
+++ script.js
@@ -1,39 +1,84 @@
-const mathActionBoxes = document.querySelectorAll('.math-action');
+const descriptionBox = document.querySelector(".action-description");
+const mathActionBoxes = document.querySelectorAll(".math-action");
 
-const firstNumBox = document.querySelector('.firstnum');
-const secondNumBox = document.querySelector('.secondnum');
+const firstNumBox = document.querySelector(".firstnum");
+const secondNumBox = document.querySelector(".secondnum");
 
-const operatorBox = document.querySelector('.operator');
+let firstNum = 0;
+let secondNum = 0;
+let result = 0;
 
-const resultBox = document.querySelector('.result');
-const input = document.querySelector('.resfield');
+const operatorBox = document.querySelector(".operator");
+
+const resultBox = document.querySelector(".result");
+const input = document.querySelector(".resfield");
 let inputValue = null;
-const mathActions = { add: '+', sub: '−', mul: '·', div: '∶' };
+const mathActions = { add: "+", sub: "−", mul: "·", div: "∶" };
 let mathActionValue = mathActions.mul;
-const tipsContainer = document.querySelector('.tips');
-const tipBoxes = document.querySelectorAll('.tip');
+const tipsContainer = document.querySelector(".tips");
+const tipBoxes = document.querySelectorAll(".tip");
+let arrForTips;
 const tips = [];
 
-mathActionBoxes.forEach(box => {
-  box.addEventListener('click', () => {
-    mathActionBoxes.forEach(b => b.classList.remove('active'));
-    box.classList.add('active');
+mathActionBoxes.forEach((box) => {
+  box.addEventListener("click", () => {
+    mathActionBoxes.forEach((b) => b.classList.remove("active"));
+    box.classList.add("active");
     mathActionValue = box.dataset.hover;
-    console.log(`box.dataset.hover = ${box.dataset.hover}`);
     operatorBox.innerText = mathActionValue;
+    descriptionBox.innerText = `виконай ${box.innerText}`;
+    hideTips();
+    setUp();
   });
 });
 
-const click = new Event('click');
-document.querySelector('.math-action.active').dispatchEvent(click);
-
-// operatorBox.innerText = mathActionValue;
+const click = new Event("click");
+document.querySelector(".math-action.active").dispatchEvent(click);
 
 function getRndNum(min = 0, max = 10) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function fillTips(res, num1, num2) {
+function setUp() {
+  let num1 = getRndNum();
+  let num2 = getRndNum();
+  console.log(`num1 = ${num1}`);
+  console.log(`num2 = ${num2}`);
+  const operator = mathActionValue;
+
+  console.log(`operator = ${operator}`);
+
+  if (operator === "+") {
+    firstNum = num1;
+    secondNum = num2;
+    result = firstNum + secondNum;
+  } else if (operator === "·") {
+    firstNum = num1;
+    secondNum = num2;
+    result = firstNum * secondNum;
+  } else if (operator === "−") {
+    num1 = getRndNum(0, 20);
+    num2 = getRndNum(0, 20);
+    firstNum = Math.max(num1, num2);
+    secondNum = Math.min(num1, num2);
+    result = firstNum - secondNum;
+  } else if (operator === "∶") {
+    const divisor = getRndNum(1, 10);
+    const quotient = getRndNum(0, 10);
+
+    firstNum = divisor * quotient;
+    secondNum = divisor;
+    result = quotient;
+  }
+
+  console.log(`firstNum = ${firstNum}`);
+  console.log(`secondNum = ${secondNum}`);
+
+  firstNumBox.innerText = firstNum;
+  secondNumBox.innerText = secondNum;
+}
+
+function showTips(num1, num2, res) {
   const numMax = Math.max(num1, num2);
   const variants = new Set([res]);
 
@@ -60,6 +105,10 @@ function fillTips(res, num1, num2) {
   tipBoxes.forEach((tip, i) => {
     tip.innerText = tipsArray[i];
   });
+
+  if (tipsContainer.classList.contains("hidden")) {
+    tipsContainer.classList.remove("hidden");
+  }
 }
 
 function shuffle(arr) {
@@ -70,32 +119,27 @@ function shuffle(arr) {
   return arr;
 }
 
-function clearTips() {
+function hideTips() {
   tipBoxes.forEach((tip, i) => {
     if (tip.innerText !== "") {
       tip.innerText = "";
     }
   });
+
+  if (!tipsContainer.classList.contains("hidden")) {
+    tipsContainer.classList.add("hidden");
+  }
 }
 
 function submit() {
-  console.log(`inputValue = ${inputValue}`);
-
   if (inputValue === result) {
     resultBox.style.backgroundColor = "green";
 
     setTimeout(function () {
-      clearTips();
+      hideTips();
+      setUp();
 
-      if (!tipsContainer.classList.contains("hidden")) {
-        tipsContainer.classList.add("hidden");
-      }
-      firstNum = getRndNum();
-      secondNum = getRndNum();
-      firstNumBox.innerText = firstNum;
-      secondNumBox.innerText = secondNum;
       resultBox.style.backgroundColor = "#e2ebf1";
-      result = firstNum * secondNum;
       input.value = "";
     }, 1000);
   } else {
@@ -103,24 +147,11 @@ function submit() {
     resultBox.style.backgroundColor = "red";
 
     setTimeout(() => {
-      if (tipsContainer.classList.contains("hidden")) {
-        tipsContainer.classList.remove("hidden");
-      }
       input.value = "";
-      fillTips(result, firstNum, secondNum);
+      showTips(firstNum, secondNum, result);
     }, 500);
   }
 }
-
-let firstNum = getRndNum();
-let secondNum = getRndNum();
-let result = firstNum * secondNum;
-
-console.log(`result = ${result}`);
-
-console.log(firstNumBox);
-firstNumBox.innerText = firstNum;
-secondNumBox.innerText = secondNum;
 
 input.addEventListener("blur", () => {
   input.focus();
